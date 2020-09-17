03d2c8831f00a282013669c0c9a82bdeff9a3f5a
Shakeb Siddiqui <sms8508@psu.edu>
2020-09-17 19:40:23 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+# Author: Shakeb Siddiqui
+# Collaborator: Samarth Tehri sqt5555@psu.edu
+# Collaborator: James Mattison jwm6532@psu.edu
+# Collaborator:  Gabriel Charpentier gbc5202@psu.edu
+# Section: 10
+# Breakout: 4
+
+def run():
+  num = int(input("Enter an int: "))
+  sum = sum_n(num)
+  print(f"sum is {sum}.")
+  word = input("Enter a string: ")
+  print_n(word, num)
+
+def sum_n(n):
+  if n <= 0:
+    return 0
+  return n + sum_n(n-1)
+
+def print_n(s, n):
+  if n <= 0:
+    return
+  print(f"{s}")
+  print_n(s, n-1)
+
+if __name__ == "__main__":
+  run()
